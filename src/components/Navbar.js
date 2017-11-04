9e21331ae58b01106340d63a58cb123a3fbc4706
linkcoding <kenny.lin.91@gmail.com>
2017-11-04 18:02:55 -0400

    added logo to src root directory

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { logOut } from '../actions/user'
 import { NavLink, Redirect } from 'react-router-dom'
 import { Menu, Image } from 'semantic-ui-react'
+import logo from './scite-icon.png'
 
 
 class Navbar extends React.Component {
@@ -26,7 +27,7 @@ class Navbar extends React.Component {
       return(
         <Menu color='teal' inverted>
           <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
-            <NavLink to="/"><img src="/public/scite-icon.png" style={{width:'29px',height:'20px'}} /></NavLink>
+            <NavLink to="/"><img src={logo} style={{width:'29px',height:'20px'}} /></NavLink>
           </Menu.Item>
           <Menu.Item name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>
             <NavLink to="/articles"> Articles </NavLink>
@@ -53,7 +54,7 @@ class Navbar extends React.Component {
       return(
         <Menu color='teal' inverted>
           <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}>
-            <Image src="/Scite-icon.png" style={{width:'29px',height:'20px'}} />
+            <Image src={logo} style={{width:'29px',height:'20px'}} />
           </Menu.Item>
           <Menu.Menu position="right">
             <Menu.Item name='signup' active={activeItem === 'signup'} onClick={this.handleItemClick}>
